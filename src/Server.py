47c7d782f268616d23e870ff4119d0ea02e16e2f
ErDiezo <58341175+ErDiezo@users.noreply.github.com>
2024-02-19 16:23:30 +0100

    Envoi/réception des données détaillés L'envoi et la réception des données ont été détaillés. On peut désormais envoyer un fichier, envoyer une commande, réceptionner les données en retour. Celles-ci sont stockées dans une variable accessible par la méthode `getReceivedData()`.

--- src/Server.py
+++ src/Server.py
@@ -57,12 +57,12 @@ class Server(threading.Thread):
     :type logger: logging.Logger
 	"""
 	def __init__(self,
-				address: List[str, int],
+				address,
 				socketFamily=socket.AF_INET,
 				socketType=socket.SOCK_STREAM,
 				socketProto=-1,
 				socketfileno=None,
-				bufferSize = 128,
+				bufferSize = 1024,
 				threadGroup=None,
 				threadTarget=None,
 				threadName=None,
@@ -70,7 +70,9 @@ class Server(threading.Thread):
 				threadKwargs=dict(),
 				threadDaemon=None,
 				logger=None):
+		
 		super().__init__(group=threadGroup, target=threadTarget, name=threadName, daemon=threadDaemon)
+
 		self._address = address
 		self._args = threadArgs
 		self._kwargs = threadKwargs
@@ -82,15 +84,17 @@ class Server(threading.Thread):
 		except Exception as e:
 			raise e
 		self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		self._socketOpen = False
-		self._connectedSocket = None
+		self._serverOpen = False
+		self._connectedSocket = None # List : 0 is the socket, 1 the adress, 2 the ID
 		self._logger = logger
+		self._receivedData = list()
     
 
 	def _open(self) -> None:
 		"""
         Opens the socket's connexion on the address given in the initialization.
 		"""
+
 		self._logger.info("Opening the server connexion...")
 		try:
 			self._socket.bind(self._address)
@@ -98,7 +102,7 @@ class Server(threading.Thread):
 		except Exception as e:
 			self._logger.error("Error while opening the connexion: %s\nSocket not opened.", e)
 		else:
-			self._socketOpen = True
+			self._serverOpen = True
 			self._logger.info("Server connexion opened. Listening on %s:%i", self._address[0], self._address[1])
 
 
@@ -106,30 +110,27 @@ class Server(threading.Thread):
 		"""
         Closes the socket.
 		"""
-		if not self._open:
+
+		if not self._serverOpen:
 			self._logger.info("The server is already closed")
 			return
 		self._logger.info("Closing connexion...")
 		self._socket.close()
-		self._socketOpen = False
+		self._serverOpen = False
 		self._socket = socket.socket(*self._socketInfos)
 		self._logger.info("Server connexion closed")
-
-	
-	def isRunning(self):
-		"""Returns True if the server is running."""
-		return self._socketOpen
     
     
 	def _accept(self) -> None:
 		"""
 		Accepts a connexion.
 		"""
+
 		while self._connectedSocket == None:
 			try:
 				conn, addr = self._socket.accept()
-				self._connectedSocket = (conn, addr)
-				self._logger.info("Accepted connexion from %s:%i", addr[0], addr[1])
+				self._connectedSocket = [conn, addr]
+				self._logger.info("Accepted connexion from %s:%d", addr[0], addr[1])
 			except socket.timeout:
 				# self._logger.debug("Timeout")
 				continue
@@ -140,46 +141,136 @@ class Server(threading.Thread):
 	def _receiveData(self) -> bytes:
 		"""
 		Receives data from the client.
+		The received data is stored in the _receivedData variable then returned.
 		"""
-		self._logger.debug("Receiving data...")
+
 		newData = self._connectedSocket[0].recv(self._bufferSize)
-		self._logger.debug(" -> [{}] {}".format(len(newData), newData))
 		allData = newData
 		while len(newData) >= self._bufferSize:
 			newData = self._connectedSocket[0].recv(self._bufferSize)
-			self._logger.debug(" -> [{}] {}".format(newData))
 			allData += newData
-		self._logger.debug(f"All received data : {allData}")
+		if allData: self._receiveData.append(allData)
 		return allData
 	
 
-	def _sendData(self, data) -> None:
+	def sendCommand(self, hw: int, cmd: int, info: int=0) -> bytes:
 		"""
-		Sends some data to the client.
+		Sends a command to the client, then receive the response from it that is added to the variable and returned.
+		hw is an integer between 0 and 1.
+		cmd is an integer between 0 and 7.
+		info is an integer between 0 and 15.
 		"""
-		self._logger.debug("Sending data...")
-		try:
-			self._connectedSocket[0].sendall(data)
-		except Exception as e:
-			self._logger.error("Error while sending data : %s", e)
-		else:
-			self._logger.debug("Data successfully sent")
+
+		# To send data use the sendData method
+		if hw == 0 and cmd == 1:
+			self._logger.warning("To send data use the sendData method")
+			return
+
+		self._sendCommand(hw, cmd, info)
+		return self._receiveData()
+	
+
+	def _sendCommand(self, hw: int, cmd: int, info: int=0) -> None:
+		"""
+		Sends a command to the client.
+		hw is an integer between 0 and 1.
+		cmd is an integer between 0 and 7.
+		info is an integer between 0 and 15.
+		"""
 		
+		# Verifying the given parameters
+		if not isinstance(hw, int) or (hw != 0 and hw != 1):
+			self._logger.error("Error while sending command (%d %d %d) : hw has to be 0 or 1.", hw, cmd, info)
+			return
+		if not isinstance(cmd, int) or cmd < 0 or cmd > 7:
+			self._logger.error("Error while sending command (%d %d %d) : cmd has to be an integer between 0 and 7 included.", hw, cmd, info)
+			return
+		if not isinstance(info, int) or info < 0 or info > 15:
+			self._logger.error("Error while sending command (%d %d %d) : info has to be an integer between 0 and 15 included.", hw, cmd, info)
+			return
+		
+		# Setting the data to send
+		toSend = bytes([(hw << 7) | (cmd << 4) | info])
+
+		# Sending the data
+		try:
+			if __name__ != "__main__":
+				self._connectedSocket[0].sendall(toSend)
+		except Exception as e:
+			self._logger.error("Error while sending command : %s", e)
+		else:
+			self._logger.info("Command %s sent", "".join([format(byte, '08b') for byte in toSend]))
+	
+
+	def sendFile(self, path : str, info : int) -> None:
+		"""
+		Sends a whole file to the client.
+		Path is the path to the file to send. It has to be binary.
+		Info is the integer data to send with the command.
+		"""
+		
+		# Verifying if the file is really a file
+		if not os.path.isfile(path):
+			self._logger.error("%s is not a file. Nothing was sent", path)
+			return
+		
+		self._logger.info("Sending file %s", path)
+
+		# Sending the file
+		amountOfDataSent = 0
+		try:
+			with open(path, "rb") as file:
+				readData = file.read(self._bufferSize - 1) # -1 for the command part
+				while readData:
+					# Send the message with the command before.
+					# The function bytes(...) converts the 1 (HW + CMD) for the higher 4 bits
+					# and the info (0..7) for the lower 4 bits into a byte.
+					# Then the data to send is concatenated, and all is sent.
+					toSend = bytes([(1 << 4) | info]) + readData
+					self._connectedSocket.sendall(bytes([(1 << 4) | info]) + toSend)
+					amountOfDataSent += len(readData)
+
+					readData = file.read(self._bufferSize - 1) # -1 for the command part
+		except Exception as e:
+			self._logger.error("Error while sending data (still %do sent) : %s", amountOfDataSent, e)
+		else:
+			self._logger.info("Data sent (%do)", bin(amountOfDataSent))
 
 
 	def run(self) -> None:
 		self._open()
 		
-		while self._connectedSocket == None: # TODO : Maybe @await ?
+		# TODO : Maybe @await ?
+		while self._serverOpen: # Server open
+			# Looking for a connexion
 			self._logger.debug("Looking for a connexion")
 			self._accept()
-			continue
+
+			if not self._connectedSocket:
+				continue
+
+			# Asks the identification to the client
+			if not self._askIdentification():
+				self._connectedSocket[0].close()
+				self._connectedSocket = []
+				self._logger.info("The connexion with %s:%d was closed because the client did not match the identification", self._connectedSocket[1][0], self._connectedSocket[1][1])
+				continue
+
+			# Check if the connexion is still open
+			while self._receiveData():
+				continue
+
+			# The connexion has been closed -> reset
+			self._connectedSocket[0].close()
+			self._connectedSocket = []
+			self._logger.info("%s:%d disconnected", self._connectedSocket[1][0], self._connectedSocket[1][1])
 
 
 	def stop(self) -> None:
 		"""
 		Closes the server.
 		"""
+
 		if self._connectedSocket:
 			self._logger.info("Closing connexion with %s:%i", self._connectedSocket[1][0], self._connectedSocket[1][1])
 			self._connectedSocket[0].close()
@@ -189,29 +280,58 @@ class Server(threading.Thread):
 		self._close()
 
 
-	def sendCommand(self, command: str, *args, **kwargs) -> None:
+	def _askIdentification(self) -> bool:
 		"""
-		Sends a command to the board.
+		Asks the identification to the client, then checks it. 
 		"""
-		pass
+
+		self._sendCommand(0, 0, 0) # Identification
+		identification = self._receiveData()
+		
+		# Checks the identification
+		# Has to be defined, here just checks if some data has been added
+		if identification:
+			return True
+		else:
+			return False
+
+	
+	def getReceivedData(self) -> bytes:
+		"""
+		Returns the list of all the data that has been received.
+		"""
+
+		return self._receivedData
+
+	
+	def isRunning(self):
+		"""
+		Returns True if the server is running.
+		"""
+
+		return self._serverOpen
 
 
 
 
 
 if __name__ == "__main__":
-	logsFiles = os.path.join("logs", "main_{}.log".format(datetime.now().strftime('%d-%m-%Y_%H:%m:%S'))) # Day + time
-	# logsFiles = os.path.join("logs", "main_{}.log".format(datetime.now().strftime('%d-%m-%Y'))) # Only day
+	import time
+	# logFile = os.path.join("logs", "Server_{}.log".format(datetime.now().strftime('%d-%m-%Y_%H:%m:%S'))) # Day + time
+	logFile = os.path.join("logs", "Server_{}.log".format(datetime.now().strftime('%d-%m-%Y'))) # Only day
 
-	handler = logging.StreamHandler()
+	handler = logging.FileHandler(logFile)
 	handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s (%(name)s): %(message)s", datefmt="%H:%M:%S"))
 	serverLogger = logging.Logger("Server")
 	serverLogger.setLevel(logging.DEBUG)
 	serverLogger.addHandler(handler)
 
-	server = Server(("192.168.1.1", 16384), logger=serverLogger, threadName="ServerThread")
+	server = Server(("0.0.0.0", 16384), logger=serverLogger, threadName="ServerThread")
 	server.start()
-	server.join()
 
+	time.sleep(2)
+	server._sendCommand(hw = 0, cmd = 7, info = 7)
+
+	server.join()
 	if server.isRunning():
 		server.stop()
