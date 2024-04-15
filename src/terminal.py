53f15be26b15b475291549fb46ccbebdaba59280
ErDiezo <58341175+ErDiezo@users.noreply.github.com>
2024-04-15 18:03:23 +0200

    Classe Terminal La classe Terminal est fonctionnelle. Elle permet d'agir comme un terminal pour entrer une commande, mais affiche l'historique des données reçues. TODO : implémenter une façon d'afficher des alertes.
    En plus, corrections mineures sur main.py et fileManager.py.

--- src/terminal.py
+++ src/terminal.py
@@ -42,22 +42,16 @@ logsFile = os.path.join("logs", "main_{}.log".format(datetime.now().strftime('%d
 # logsFile = os.path.join("logs", "main_{}.log".format(datetime.now().strftime('%d-%m-%Y'))) # Only day
 
 # Defining the colors used by the environment. Each color is matched to an integer, and has its proper object
-# (see the funciont _initializeColors in the FilesManager class)
+# (see the funciont _initializeColors in the Terminal class)
 COLORS = {
 	# The indexes of each color, used to create the objects
 	"index" : {
 		"text" : 0,
-		"file" : 0,
-		"dir" : 1,
-		"path" : 3, # Same as alert
 		"alert" : 3
 	},
 	# The objects used to display some text. Defined here for more clarity
 	"curses" : {
 		"text" : None,
-		"file" : None,
-		"dir" : None,
-		"path" : None,
 		"alert" : None
 	}
 }
@@ -67,9 +61,9 @@ COLORS = {
 # ===========================================================================================
 # ****************************************** CLASS ******************************************
 # ===========================================================================================
-# Variable declaration/definition
+# Class declaration/definition
 
-class FilesManager(threading.Thread):
+class Terminal(threading.Thread):
 	"""
     Represents a files manager. Allows to navigate through the tree, and select a file.
 	The parameter thread indicates if the class has to be used threaded or not.
@@ -103,16 +97,21 @@ class FilesManager(threading.Thread):
 		# Initialization of the values used for the curses environment
 		self._stdscr = None
 		self._currentDir = os.getcwd()
-		self._files = sorted(os.listdir(self._currentDir))
-		self._selectedIndex = 0
 		self._offset = 0
-		self._availableLines = 0
-		self._chosenFile = None
+
+		self._connectedClient = "" # The id to the client, shown in the header
 
 		self._running = False
+		self._input = "" # A string containing the user input
+		self._cursorXPos = 0 # The horizontal position of the cursor, relatively to the beginning of the self._input string
+
+		self._history = [] 	# A list that contains tuples of the command and the associated data:
+							# Example : [(command1, data1), (command2, data2)]
+
+		self.commands = [] # A list of all commands that have to be sent
 
 
-	def run(self) -> str:
+	def run(self):
 		"""
 		This function is almost a copy/paste of the curses.wrapper function (see https://github.com/python/cpython/blob/3.12/Lib/curses/__init__.py).
 		Doing this allows the curses environment to be used inside of a threaded class.
@@ -120,7 +119,7 @@ class FilesManager(threading.Thread):
 		However, it returns the path to the selected file.
 		"""
 
-		self._logger.info("Starting files manager")
+		self._logger.info("Starting terminal")
 
 		# Initialize curses
 		self._stdscr = curses.initscr()
@@ -145,16 +144,16 @@ class FilesManager(threading.Thread):
 			self._logger.warning("Could not initialize colors")
 		else:
 			self._initializeColors()
+
+		# Turn on instant key response
+		# self._stdscr.nodelay(1)
 		
 		self._logger.info("Files manager started. Start of the main loop")
 
-		try:
-			return self._mainLoop()
-		except curses.error as e:
-			filesManagerLogger.error("The terminal might be too small. Try to make it bigger (Error : %s)", e)
+		self._mainLoop()
 
 
-	def _mainLoop(self) -> str:
+	def _mainLoop(self):
 		"""
 		The mainloop of the curses envrionment.
 		It returns the path to the selected file.
@@ -162,50 +161,57 @@ class FilesManager(threading.Thread):
 		self._running = True
 		
 		# Hide the cursor
-		curses.curs_set(0)
+		# curses.curs_set(0)
 
 		# Display before starting the loop
 		self._display()
 
 		while self._running:
+			# Move the cursor
+			self._stdscr.move(curses.LINES - 1, 3 + self._cursorXPos)
+
+			# Refresh to allow the user to see the cursor
+			self._stdscr.refresh()
+			
 			# Get the key pressed
 			key = self._stdscr.getch()
 
-			# Actions depending on the key pressed
-			if key == ord('q'): # Quit the program
-				break
-			elif key == ord('a'): # Test
-				self._alert("test")
-			elif key == curses.KEY_UP and self._selectedIndex > 0: # Navigate threw the tree
-				self._selectedIndex -= 1
-				if (self._selectedIndex < self._offset):
-					self._offset -= 1
-			elif key == curses.KEY_DOWN and self._selectedIndex < len(self._files) - 1: # Navigate threw the tree
-				self._selectedIndex += 1
-				if (self._selectedIndex > self._availableLines - 1 + self._offset):
-					self._offset += 1
-			elif key == curses.KEY_LEFT:
-				self._changeDir(os.path.dirname(self._currentDir))
-			elif key == curses.KEY_RIGHT:
-				self._onKeyRight()
-			elif key == curses.KEY_ENTER or key == 10 or key == 13: # ENTER or \n or \r
-				if self._onEnterPress():
-					break
-			# TODO : one key to press to enter the full path with the keyboard
+			if key != curses.ERR:
+				if key == curses.KEY_BACKSPACE:
+					if self._input:
+						self._input = self._input[:-1]
+						self._cursorXPos -= 1
+				elif key == curses.KEY_DC:
+					self._input = self._input[:self._cursorXPos] + self._input[self._cursorXPos+1:]
+				elif key == curses.KEY_ENTER or key == 10:  # 10 is the ASCII code for Enter
+					self._onEnterPress()
+				elif key == curses.KEY_LEFT:
+					if self._cursorXPos > 0:
+						self._cursorXPos -= 1
+				elif key == curses.KEY_RIGHT:
+					if self._cursorXPos < len(self._input):
+						self._cursorXPos += 1
+				elif key == 258 or key == 259: # Scrolling
+					pass
+				else:
+					keyChar = chr(key)
+					if keyChar.isprintable():
+						# Prevent to enter something larger than the sceen size
+						if self._cursorXPos < curses.COLS - 4:
+							self._input += str(keyChar)
+							self._cursorXPos += 1
 
+			# Global display
 			self._display()
 
 		# End of the file manager
 		self._logger.info("Ending files manager")
 		# Set everything back to normal
 		self._stdscr.keypad(0)
+		curses.curs_set(1)
 		curses.echo()
 		curses.nocbreak()
 		curses.endwin()
-
-		if self._chosenFile:
-			self._logger.info("File chosen : %s", self._chosenFile)
-		return self._chosenFile
 	
 
 	def _initializeColors(self) -> None:
@@ -217,15 +223,11 @@ class FilesManager(threading.Thread):
 		# ********** Basic colors ********** 
 		# Creates pairs linked to an integer
 		# They represent the foreground color, then the background color
-		curses.init_pair(COLORS["index"]["dir"], curses.COLOR_CYAN, curses.COLOR_BLACK) # Directory
 		curses.init_pair(COLORS["index"]["alert"], curses.COLOR_RED, curses.COLOR_BLACK) # Alert
 
 		# Match the color object on the color pair
 		# These are the objects used when displaying some text
 		COLORS["curses"]["text"] = curses.color_pair(COLORS["index"]["text"])
-		COLORS["curses"]["file"] = curses.color_pair(COLORS["index"]["file"])
-		COLORS["curses"]["dir"] = curses.color_pair(COLORS["index"]["dir"])
-		COLORS["curses"]["path"] = curses.color_pair(COLORS["index"]["path"])
 		COLORS["curses"]["alert"] = curses.color_pair(COLORS["index"]["alert"])
 
 		# ********** Alert colors **********
@@ -250,156 +252,125 @@ class FilesManager(threading.Thread):
 
 		# ---------- Header ----------
 		headerLines = 2
-		self._stdscr.addstr(0, 0, "path :", COLORS["curses"]["text"])
-		# If too large for the screen (size of "path : ..." + 1), shortens the path
-		if (len(self._currentDir)) < width-11:
-			self._stdscr.addstr(0, 7, self._currentDir, COLORS["curses"]["path"])
+		self._stdscr.attron(curses.A_BOLD)
+		if self._connectedClient:
+			if len(text) < width-11:
+				self._stdscr.addstr(0, (width - len(text)) // 2, text, COLORS["curses"]["text"] | curses.A_BOLD)
+			elif len(self._connectedClient) < width-11:
+				# If too large for the screen display only the id
+				self._stdscr.addstr(0, (width - len(self._connectedClient)) // 2, self._connectedClient, COLORS["curses"]["text"] | curses.A_BOLD)
+			else:
+				self._stdscr.addstr(0, (width - 9) // 2, "Connected", COLORS["curses"]["text"] | curses.A_BOLD)
 		else:
-			self._stdscr.addstr(0, 7, "...{}".format(self._currentDir[len(self._currentDir)-width+11:]), COLORS["curses"]["path"])
+			self._stdscr.addstr(0, (width - 13) // 2, "Not connected", COLORS["curses"]["text"] | curses.A_BOLD)
+
 		self._drawHorizontalLine(1)
 
 		# ---------- Footer ----------
 		footerLines = 2
-		self._drawHorizontalLine(height-2)
-		footer = "[q] Quit    [<] Go out    [>] Go in    [ENTER] Send file"
-		if width > 56: # len(footer) 
-			self._stdscr.addstr(height-1, 0, "[q] Quit    [<] Go out    [>] Go in    [ENTER] Send file", COLORS["curses"]["text"])
-		else:
-			self._stdscr.addstr(height-1, 0, "[q]    [<]    [>]    [ENTER]", COLORS["curses"]["text"])
-
+		self._drawHorizontalLine(height - 2)
+		self._stdscr.addstr(height-1, 0, ">> " + self._input, COLORS["curses"]["text"])
+		
 		# ---------- Main ----------
-		self._availableLines = height - headerLines - footerLines
-		# The offset allows to scrool the list if it is too long to be shown in the entire screen
-		offset = max(0, self._selectedIndex - self._availableLines + 1)
-		for i in range(min(len(self._files), self._availableLines)):
-			currentFile = os.path.join(self._currentDir, self._files[i + self._offset])
-			if os.path.isdir(currentFile):
-				self._stdscr.addstr(i + headerLines, 0, self._files[i + self._offset], COLORS["curses"]["dir"] | curses.A_BOLD | curses.A_REVERSE*((i + self._offset)==self._selectedIndex))
+		availableLines = height - headerLines - footerLines
+		# Define the maximum width available for text (considering indentation)
+		maxWidth = width - 5
+
+		linesCount = 0
+		textToDisplay = []
+
+		for command, text in reversed(self._history):
+			if text:
+				# Split text into lines if it exceeds the maximum width
+				lines = reversed([text[i:i+maxWidth] for i in range(0, len(text), maxWidth)])
+
+				for line in lines:
+					textToDisplay.append(line)
+					linesCount += 1
+					if linesCount >= availableLines:
+						break
+			
+			if linesCount >= availableLines:
+				break
+			textToDisplay.append("*BOLD*" + command + ":")
+			linesCount += 1
+			if linesCount >= availableLines:
+				break
+		
+		textToDisplay.reverse()
+		self._stdscr.move(headerLines, 0)
+		for idx, line in enumerate(textToDisplay):
+			if line[:6] == "*BOLD*":
+				self._stdscr.attron(curses.A_BOLD)
+				self._stdscr.addstr(line[6:] + '\n')
+				self._stdscr.attroff(curses.A_BOLD)
 			else:
-				self._stdscr.addstr(i + headerLines, 0, self._files[i + self._offset], COLORS["curses"]["file"] | curses.A_REVERSE*((i + self._offset)==self._selectedIndex))
+				self._stdscr.addstr("    " + line)
+				if idx < len(textToDisplay) - 1:
+					self._stdscr.addch('\n')
+
 
 		self._stdscr.refresh()
-	
 
-	def _alert(self,
-			subText1 = "Do you really want to send this file?",
-			subText1Replacement = "Send this file?",
-			subText2 = ""
-			) -> bool:
+
+	def _onEnterPress(self) -> None:
 		"""
-		This function is used to display an alert in the center of the screen
+		Executed when the user presses ENTER
 		"""
+		if self._input == "exit":
+			self.stop()
+			return
 
-		response = False
+		# Process the command
+		inputList = self._input.split(' ')
+		self._history.append((inputList[0], ' '.join(inputList[1:]) if len(inputList) > 1 else None))
+		self._input = ""
+		self._cursorXPos = 0
 
-		oldHeight, oldWidth = self._stdscr.getmaxyx()
-		nLines = 7 # 5 lines + 2 for borders
-		nCols = min(oldWidth, max(len(subText1) + 4, len(subText1) + 2, len(subText2))) # + 2 for borders + 2 for padding
-
-		xMin = (oldWidth - nCols) // 2
-		yMin = (oldHeight - nLines) // 2
-
-		alertWin = curses.newwin(nLines, nCols, yMin, xMin)
-		
-		while True:
-
-			alertWin.clear()
-
-			# All the lines to show
-			alertWin.addstr(1, (nCols - 5) // 2, "ALERT", curses.A_BOLD | COLORS["curses"]["alert"])
-			if nCols >= len(subText1) + 2:
-				alertWin.addstr(2, (nCols - len(subText1)) // 2, subText1)
-			else:
-				alertWin.addstr(2, (nCols - len(subText1Replacement)) // 2, subText1Replacement)
-			alertWin.addstr(3, (nCols - len(subText2)) // 2, subText2, curses.A_ITALIC)
-			# Empty line
-			alertWin.addstr(5, (nCols - 2) // 3, "No", curses.A_REVERSE*(not response))
-			alertWin.addstr(5, (nCols - 3) // 3 * 2, "Yes", curses.A_REVERSE*response)
-
-			alertWin.border() # Border included in the window size.
-
-			alertWin.refresh()
-
-			key = self._stdscr.getch()
-
-			if key == ord('q'):
-				response = False
-				break
-			elif key == ord('n'):
-				response = False
-				break
-			elif key == ord('y'):
-				response = True
-				break
-			elif key == curses.KEY_RIGHT:
-				response = True
-			elif key == curses.KEY_LEFT:
-				response = False
-			elif key == curses.KEY_ENTER or key == 10 or key == 13: # ENTER or \n or \r
-				break
-		
-		return response
-
-	
-	def _onKeyRight(self) -> None:
-		"""
-		Defines what happens when the user press the right key.
-		"""
-
-		currentFile = os.path.join(self._currentDir, self._files[self._selectedIndex])
-		if os.path.isdir(currentFile):
-			self._changeDir(currentFile)
-
-
-	def _onEnterPress(self) -> bool:
-		"""
-		Defines what happens when the user press the ENTER key.
-		Returns the path to the file if a file is selected.
-		"""
-
-		currentFile = os.path.join(self._currentDir, self._files[self._selectedIndex])
-		if os.path.isdir(currentFile):
-			self._changeDir(currentFile)
-		elif self._alert(subText2 = self._files[self._selectedIndex]):
-			self._chosenFile = currentFile
-			return True
-
-		return False
-
-
-	def _changeDir(self, dirPath: str) -> None:
-		"""
-		Changes the working directory.
-		"""
-		self._stdscr.clear()
-		self._currentDir = dirPath
-		self._selectedIndex = 0
-		self._offset = 0
-		self._files = sorted(os.listdir(self._currentDir))
-		self._logger.debug("Current directory changed : %s", self._currentDir)
-		
 
 	def stop(self) -> None:
 		"""
 		Stops the files manager.
 		"""
 		self._running = False
+		self._stdscr.clear()
 
 
 
 
 if __name__ == "__main__":
-	# logFile = os.path.join("logs", "FilesManager_{}.log".format(datetime.now().strftime('%d-%m-%Y_%H:%m:%S'))) # Day + time
-	logFile = os.path.join("logs", "FilesManager_{}.log".format(datetime.now().strftime('%d-%m-%Y'))) # Only day
+	# logsFile = os.path.join("logs", "Terminal_{}.log".format(datetime.now().strftime('%d-%m-%Y_%H:%m:%S'))) # Day + time
+	logsFile = os.path.join("logs", "Terminal_{}.log".format(datetime.now().strftime('%d-%m-%Y'))) # Only day
 
-	handler = logging.FileHandler(logFile)
+	handler = logging.FileHandler(logsFile)
 	handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s (%(name)s): %(message)s", datefmt="%H:%M:%S"))
 	handler.setLevel(logging.DEBUG)
 	
-	filesManagerLogger = logging.Logger("FilesManager")
-	filesManagerLogger.addHandler(handler)
+	terminalLogger = logging.Logger("Terminal")
+	terminalLogger.addHandler(handler)
 
-	filesManager = FilesManager(logger=filesManagerLogger)
+	terminal = Terminal(logger=terminalLogger)
+
+	terminal._history = [
+        ("KEYWORD1", "some text corresponding to keyword 1 that can be longer than the width of the screen so it has to be split in lines"),
+        ("KEYWORD2", "some text corresponding to keyword 2 that can be longer than the width of the screen so it has to be split in lines"),
+        ("KEYWORD3", "some text corresponding to keyword 3 that can be longer than the width of the screen so it has to be split in lines"),
+        ("KEYWORD4", "some text corresponding to keyword 4 that can be longer than the width of the screen so it has to be split in lines"),
+        ("KEYWORD5", "some text corresponding to keyword 5 that can be longer than the width of the screen so it has to be split in lines"),
+        ("KEYWORD6", "some text corresponding to keyword 6 that can be longer than the width of the screen so it has to be split in lines"),
+        ("KEYWORD7", "some text corresponding to keyword 7 that can be longer than the width of the screen so it has to be split in lines"),
+        ("KEYWORD8", "some text corresponding to keyword 8 that can be longer than the width of the screen so it has to be split in lines"),
+        ("KEYWORD9", "some text corresponding to keyword 9 that can be longer than the width of the screen so it has to be split in lines"),
+        ("KEYWORD10", "some text corresponding to keyword 10 that can be longer than the width of the screen so it has to be split in lines"),
+        ("KEYWORD11", "some text corresponding to keyword 11 that can be longer than the width of the screen so it has to be split in lines"),
+        ("KEYWORD12", "some text corresponding to keyword 12 that can be longer than the width of the screen so it has to be split in lines"),
+        ("KEYWORD13", "some text corresponding to keyword 13 that can be longer than the width of the screen so it has to be split in lines"),
+        ("KEYWORD14", "some text corresponding to keyword 14 that can be longer than the width of the screen so it has to be split in lines"),
+        # Add more data as needed
+    ]
 	
-	filesManager.start()
-	filesManager.join()
+	terminal.start()
+	terminal.join()
+
+	with open(logsFile, "+a") as f:
+		f.write("\n")
